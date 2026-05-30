6c5318fe5112fb03082731d48d13c6350bd26da0
GnoxNahte <gnoxnahte.dev@gmail.com>
2026-05-30 11:14:54 +0800

    test server island

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
   adapter: vercel(),
   prefetch: {
     prefetchAll: true,
